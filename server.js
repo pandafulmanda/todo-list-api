08ecb1ee1d5f0638c8c0a117e40e8861f77983a8
amanda <pandafulmanda@gmail.com>
2019-09-21 11:42:49 -0500

    api with get, post, put

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var uuid = require('uuid/v1');
 
 var app = express();
 app.use(bodyParser.json());
@@ -7,24 +8,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var todoList = [
     {
-        id: 1,
+        id: uuid(),
         todo: "Implement a REST API"
     }
 ];
 
+function fetchTodoById(id) {
+    const todoItem = todoList.find(function(todoItem) {
+        return todoItem.id.toString() === id
+    })
+
+    return todoItem
+}
+
 // GET /api/todos
 // This should respond with the full list of todo items.
+app.get('/api/todos', function (request, response, nextFn) {
+    response.send(todoList)
+})
 
 // GET /api/todos/:id
 // This should respond with the information for the matching todo item
 // by id.
 // If the matching todo does not exist, the server should respond
 // with a 404 status code.
+app.get('/api/todos/:id', function (request, response, nextFn) {
+    const todoItem = fetchTodoById(request.params.id)
+
+    if (todoItem !== undefined) {
+        response.send(todoItem)
+    } else {
+        response.sendStatus(404)
+    }
+})
+
 
 // POST /api/todos
 // This should take the body of the request and add it to todoList.
 // Remember to generate a unique id for the new todo item.
 // This endpoint should respond with the new item with it's id.
+app.post('/api/todos', function(request, response, nextFn) {
+
+    const newItem = {
+        id: uuid(),
+        todo: request.body.todo
+    }
+    todoList.push(newItem)
+
+    response.send(newItem)
+})
+
 
 // PUT /api/todos/:id
 // This should update the matching todo item by id with the
@@ -32,6 +65,23 @@ var todoList = [
 // updated item.
 // If the matching todo does not exist, the server should respond
 // with a 404 status code.
+app.put('/api/todos/:id', function (request, response, nextFn) {
+    const todoItem = fetchTodoById(request.params.id)
+    
+    if (todoItem === undefined) {
+        response.sendStatus(404)
+        return
+    }
+
+    const updates = request.body
+    const updatedTodo = Object.assign(todoItem, updates)
+
+    // console.log(updatedTodo, 'updatedTodo')
+    // console.log(todoList, 'todoList')
+
+    response.send(updatedTodo)
+})
+
 
 // DELETE /api/todos/:id
 // This should remove the matching item from the list of todo items.
